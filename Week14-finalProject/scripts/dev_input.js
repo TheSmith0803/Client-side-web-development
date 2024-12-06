7cb3c461b79b2b0f5478b33b8fa686df01893a53
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-06 13:31:56 -0500

    conditional for 1 time submit and fade in animation

--- Week14-finalProject/scripts/dev_input.js
+++ Week14-finalProject/scripts/dev_input.js
@@ -1,9 +1,50 @@
 //this script is for data collection on the form of
 
-let emails = []
-let names = []
-let comments = []
+
 
 $(document).ready(function() {
+    //arrays to store data for submission
     
+    let submissions = [];
+    let clicked = false;
+
+    $("#dev-form-container").submit(function(event) {
+        event.preventDefault();
+        
+        //conditional to make sure submission can only happen once
+        //per page load
+
+        if (!clicked) {
+            //get input from form
+            let email = $("#email-input").val();
+            let name = $("#name-input").val();
+            let comment = $("#message-input").val();
+
+            let submission = {
+                email: email,
+                name: name,
+                comment: comment
+            };
+
+            //write submission to outer array for processing
+            submissions.push(submission);
+
+            //clear inputs
+            $("#email-input").val('');
+            $("#name-input").val('');
+            $("#comment-input").val('');
+
+            console.log(submissions);
+
+            let message = $('<div class="submit-message" style="text-align: center; font-size: 18px; margin: 15px 0px; transistion: 0.5s ease-in;">Thank you for sharing ' + name + "!<br>I will reach out to you soon!</div>");
+
+            $("#dev-form-button").after(message.hide());
+            message.fadeIn(500);
+
+            clicked = true;
+        } else {
+            //do nothing
+        }
+
+    });
 });
