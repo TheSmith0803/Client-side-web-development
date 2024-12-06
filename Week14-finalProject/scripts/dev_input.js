81e337a592049570ef2b5cfe35b5d86e6aa0a454
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-06 13:52:50 -0500

    i forgor semicolon

--- Week14-finalProject/scripts/dev_input.js
+++ Week14-finalProject/scripts/dev_input.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
             clicked = true;
         } else {
-            $('.submit-message').html("You can only submit once!")
+            $('.submit-message').html("You can only submit once!");
         }
 
     });
