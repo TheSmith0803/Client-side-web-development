9ac5a4bc834654d83f562cc05d3e0be1d6d61328
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-10 10:58:04 -0500

    script to change text on album hover

---
+++ Week14-finalProject/scripts/album-hover.js
@@ -0,0 +1,35 @@
+//quick script for changing styles
+//on music page
+
+$(document).ready(function() {
+    
+    $('.q-img').hover(
+        function() {
+            $('.q-text').stop(true, false).css("color", "white").animate({
+                paddingTop: "10px",
+                fontSize: "37px"
+            }, 300);
+        },
+        function() {
+            $('.q-text').stop(true, false).css("color", "").animate({
+                paddingTop: "0px",
+                fontSize: "25px"
+            }, 800);
+        }
+    );
+
+    $('.b-img').hover(
+        function() {
+            $('.b-text').stop(true, false).css("color", "white").animate({
+                paddingTop: "10px",
+                fontSize: "37px"
+            }, 300);
+        },
+        function() {
+            $('.b-text').stop(true, false).css("color", "").animate({
+                paddingTop: "0px",
+                fontSize: "25px"
+            }, 800);
+        }
+    );
+});
